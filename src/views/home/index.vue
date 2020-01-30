81309eb9f27d78f723ca5084079f0d540813bef3
allenxz <13226271061@163.com>
2020-01-30 13:29:56 +0800

    制作home主页面

---
+++ src/views/home/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <layout></layout>
+</template>
+
+<script>
+import './style.scss'
+import Layout from '@/components/layout'
+export default {
+  name: 'home',
+  data () {
+    return {
+      collapsed: false
+    }
+  },
+  components: {
+    Layout
+  }
+}
+</script>
