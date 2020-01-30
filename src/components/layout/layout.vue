82d5d1a1a03e0e2eb3b5e716f11ce78d959b9f1e
allenxz <13226271061@163.com>
2020-01-30 13:29:17 +0800

    制作layout通用组件

---
+++ src/components/layout/layout.vue
@@ -0,0 +1,33 @@
+<template>
+  <a-layout id="home">
+    <sider :collapsed="collapsed"></sider>
+    <a-layout>
+      <a-layout-header style="background: #fff; padding: 0">
+        <a-icon
+          class="trigger"
+          :type="collapsed ? 'menu-unfold' : 'menu-fold'"
+          @click="()=> collapsed = !collapsed"
+        />
+      </a-layout-header>
+      <a-layout-content :style="{ margin: '24px 16px', padding: '24px', background: '#fff', minHeight: '280px' }">
+        <router-view></router-view>
+      </a-layout-content>
+    </a-layout>
+  </a-layout>
+</template>
+
+<script>
+import './style.scss'
+import Sider from '@/components/sider'
+export default {
+  name: 'home',
+  data () {
+    return {
+      collapsed: false
+    }
+  },
+  components: {
+    Sider
+  }
+}
+</script>
