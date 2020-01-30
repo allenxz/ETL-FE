bd6cfabc70b746875a342c718eaa691be6e14885
allenxz <13226271061@163.com>
2020-01-30 23:51:10 +0800

    完成sider静态部分的制作

--- src/components/sider/sider.vue
+++ src/components/sider/sider.vue
@@ -1,19 +1,30 @@
 <template>
   <a-layout-sider :trigger="null" collapsible v-model="collapsed">
     <div class="logo" />
-    <a-menu theme="dark" mode="inline" :defaultSelectedKeys="['1']">
-      <a-menu-item key="1">
-        <a-icon type="user" />
-        <span>nav 1</span>
-      </a-menu-item>
-      <a-menu-item key="2">
-        <a-icon type="video-camera" />
-        <span>nav 2</span>
-      </a-menu-item>
-      <a-menu-item key="3">
-        <a-icon type="upload" />
-        <span>nav 3</span>
-      </a-menu-item>
+    <a-menu
+      :defaultSelectedKeys="['1']"
+      :defaultOpenKeys="['sub1']"
+      mode="inline"
+      theme="dark"
+      :inlineCollapsed="collapsed">
+      <a-sub-menu key="sub1">
+        <span slot="title"><a-icon type="fund" /><span>监控告警</span></span>
+        <a-menu-item key="1">Kafka监测</a-menu-item>
+      </a-sub-menu>
+      <a-sub-menu key="sub2">
+        <span slot="title"><a-icon type="interation" /><span>数据处理</span></span>
+        <a-menu-item key="2">流程管理</a-menu-item>
+        <a-menu-item key="3">部署管理</a-menu-item>
+      </a-sub-menu>
+      <a-sub-menu key="sub3">
+        <span slot="title"><a-icon type="database" /><span>数据储存</span></span>
+        <a-menu-item key="4">数据结构</a-menu-item>
+        <a-menu-item key="5">Kafka管理</a-menu-item>
+      </a-sub-menu>
+      <a-sub-menu key="sub4">
+        <span slot="title"><a-icon type="cluster" /><span>参数配置</span></span>
+        <a-menu-item key="6">待定</a-menu-item>
+      </a-sub-menu>
     </a-menu>
   </a-layout-sider>
 </template>
