89c0a988b0d2f99f0264e13f1fae8b29cc8bb6bb
allenxz <13226271061@163.com>
2020-04-10 01:02:01 +0800

    完成任务报告页的基本页面

--- src/views/report/show/index.vue
+++ src/views/report/show/index.vue
@@ -1,5 +1,58 @@
 <template>
-  <div>dsd</div>
+  <div class="report-show">
+    <figure class="card " style="--image-src: url('https://source.unsplash.com/78A265wPiO4')">
+      <figcaption>
+        <span class="info">
+          <h3>The Hills</h3>
+          <span>by David Marcu</span>
+        </span>
+        <span class="links">
+          <a href="#"><i class="fas fa-heart"></i></a>
+          <a href="#" target="_blank"><i class="fab fa-instagram"></i></a>
+          <a href="https://unsplash.com/photos/78A265wPiO4" target="_blank"><i class="fas fa-share"></i></a>
+        </span>
+      </figcaption>
+    </figure>
+    <figure class="card " style="--image-src: url('https://source.unsplash.com/78A265wPiO4')">
+      <figcaption>
+        <span class="info">
+          <h3>The Hills</h3>
+          <span>by David Marcu</span>
+        </span>
+        <span class="links">
+          <a href="#"><i class="fas fa-heart"></i></a>
+          <a href="#" target="_blank"><i class="fab fa-instagram"></i></a>
+          <a href="https://unsplash.com/photos/78A265wPiO4" target="_blank"><i class="fas fa-share"></i></a>
+        </span>
+      </figcaption>
+    </figure>
+    <figure class="card " style="--image-src: url('https://source.unsplash.com/78A265wPiO4')">
+      <figcaption>
+        <span class="info">
+          <h3>The Hills</h3>
+          <span>by David Marcu</span>
+        </span>
+        <span class="links">
+          <a href="#"><i class="fas fa-heart"></i></a>
+          <a href="#" target="_blank"><i class="fab fa-instagram"></i></a>
+          <a href="https://unsplash.com/photos/78A265wPiO4" target="_blank"><i class="fas fa-share"></i></a>
+        </span>
+      </figcaption>
+    </figure>
+    <figure class="card" style="--image-src: url('https://source.unsplash.com/78A265wPiO4')">
+      <figcaption>
+        <span class="info">
+          <h3>The Hills</h3>
+          <span>by David Marcu</span>
+        </span>
+        <span class="links">
+          <a href="#"><i class="fas fa-heart"></i></a>
+          <a href="#" target="_blank"><i class="fab fa-instagram"></i></a>
+          <a href="https://unsplash.com/photos/78A265wPiO4" target="_blank"><i class="fas fa-share"></i></a>
+        </span>
+      </figcaption>
+    </figure>
+  </div>
 </template>
 
 <script>
